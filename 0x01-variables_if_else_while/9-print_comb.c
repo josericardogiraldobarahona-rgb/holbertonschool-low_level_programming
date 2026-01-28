76c5c07c49b1515c5f3af6a1f02640b1bb56338f
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-01-28 17:14:07 -0500

    language c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* description prinfs the alphabet in lowercase using putchar
+* Return: sempre 0 (exito)
+*/
+
+int main(void)
+{
+int numero;
+for (numero = '0'; numero <= '9'; numero++)
+{
+putchar(numero);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
