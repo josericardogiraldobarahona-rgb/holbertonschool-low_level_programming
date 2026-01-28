f04ed0f11e42f92138ed0e6b39c0d1cfdf41224d
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-01-28 16:03:53 -0500

    language c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* description prinfs the alphabet in lowercase using putchar
+* Return: sempre 0 (exito)
+*/
+
+int main(void)
+{
+char letra;
+for (letra = 'a'; letra <= 'z'; letra++)
+{
+putchar(letra);
+}
+putchar('\n');
+return (0);
+}
