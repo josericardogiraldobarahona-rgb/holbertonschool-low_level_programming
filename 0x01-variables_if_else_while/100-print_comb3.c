5ad55a2c3d2cbfdc62d131737ec9cabbaeb4deb5
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-01-29 15:47:30 -0500

    language c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,7 +12,7 @@ int numero;
 int numero2;
 for (numero = '0'; numero <= '8'; numero++)
 {
-for (numero2 = '0'; numero2 <= '9'; numero2++)
+for (numero2 = numero +1; numero2 <= '9'; numero2++)
 {
 if (numero2 != '0' && numero2 != numero)
 {
