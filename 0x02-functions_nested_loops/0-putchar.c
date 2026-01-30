1f8c8ce02e3ffee99e3e7893d6949b76db4fc343
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-01-30 16:24:14 -0500

    funciones en c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,15 @@
 * Return: sempre 0 (exito)
 */
 
-int main(void){
+int main(void)
+{
 char *texto = "_putchar";
 int i = 0;
- while (texto[i] != '\0')
- {
-  _putchar(texto[i]);
-  i++;
- }
+while (texto[i] != '\0')
+{
+_putchar(texto[i]);
+i++;
+}
 _putchar('\n');
 return (0);
 }
