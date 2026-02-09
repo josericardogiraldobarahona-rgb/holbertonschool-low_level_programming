abe6abb8153b7c357cefdfe90d8233cb98fed1eb
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-02-09 16:03:53 -0500

    funciones

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,13 +5,16 @@
 * alfabeto en minusculas seguido de una nueva linea
 * Return: sempre 0 (exito)
  */
+int i;
 void print_alphabet_x10(void)
 {
 char letra = 'a';
+for (i=0 ; i<10 ; i++){
 while (letra <= 'z')
 {
 _putchar(letra);
 letra++;
+}
 }
 _putchar('\n');
 return;
