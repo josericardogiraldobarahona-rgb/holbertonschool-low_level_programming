e3d4f66634841e931ffc8b3c429460ba714d6bf8
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-02-09 16:07:19 -0500

    funciones

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_alphabet- imprime el
-* alfabeto en minusculas seguido de una nueva linea
+* print_alphabet_x10- imprime el
+* alfabeto en minusculas seguido de una nueva linea 10 veces
 * Return: sempre 0 (exito)
  */
-int i;
+
 void print_alphabet_x10(void)
 {
+int i;
 char letra = 'a';
-for (i=0 ; i<10 ; i++){
+for (i = 0 ; i < 10 ; i++)
+{
 while (letra <= 'z')
 {
 _putchar(letra);
