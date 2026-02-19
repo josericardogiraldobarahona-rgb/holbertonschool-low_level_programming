e43eefff4366548aae44920908632ed5c277ed79
Jose Ricardo <josericardogiraldobarahona@gmail.com>
2026-02-19 15:20:24 -0500

    numero4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* _isalpha comprueba si el caracter es  una letra 
-*ya sea mayuscula o minuscula _isalpha(int c);
+* _isalpha verifica si es caracter
+* ya sea mayuscula o minuscula  int _isalpha(int c);
 * Return: 1 si es letra 0 si no es el caso  (exito)
 */
 
